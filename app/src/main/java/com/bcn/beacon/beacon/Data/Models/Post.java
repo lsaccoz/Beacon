0fb140f7a7069547c8a7fc803096097087c28f4c
Neema <commodornim@googlemail.com>
2016-10-16 23:49:28 -0700

    added basic data models

---
+++ app/src/main/java/com/bcn/beacon/beacon/Data/Models/Post.java
@@ -0,0 +1,72 @@
+package com.bcn.beacon.beacon.Data.Models;
+
+/**
+ * Created by neema on 2016-10-16.
+ */
+public class Post {
+
+    private Long _id;
+    private String uuid;
+    private String userId;
+    private String text;
+    private String photo;
+    private String dateId;
+    private String[] likeIds;
+    private String[] commentIds;
+
+    public String getId(){
+        return uuid;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+
+    public String getPhoto(){
+        return photo;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public String getDateId(){
+        return dateId;
+    }
+
+    public String[] getLikeIds(){
+        return likeIds;
+    }
+
+    public String[] getCommentIds(){
+        return commentIds;
+    }
+
+    public void setId(String uuid){
+        this.uuid = uuid;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void setPhoto(String photo){
+        this.photo = photo;
+    }
+
+    public void setDateId(String dateId){
+        this.dateId = dateId;
+    }
+
+    public void setLikeIds(String[] likeIds){
+        this.likeIds = likeIds;
+    }
+
+    public void setCommentIds(String[] commentIds){
+        this.commentIds = commentIds;
+    }
+}
