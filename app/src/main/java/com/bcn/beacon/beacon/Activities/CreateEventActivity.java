b2b03f653e43f84c542568e8f7d6db66d2307f97
Omar <odzick@gmail.com>
2016-11-01 10:55:40 -0700

    add spinner for category, image from camera/library

--- app/src/main/java/com/bcn/beacon/beacon/Activities/CreateEventActivity.java
+++ app/src/main/java/com/bcn/beacon/beacon/Activities/CreateEventActivity.java
@@ -4,13 +4,18 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.media.ExifInterface;
+import android.provider.OpenableColumns;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,10 +23,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.graphics.Bitmap;
@@ -56,10 +64,11 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.joanzapata.iconify.widget.IconTextView;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 import static android.support.design.R.styleable.View;
 
-public class CreateEventActivity extends AppCompatActivity {
+public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText eTime;
     EditText eDate;
@@ -68,8 +77,11 @@ public class CreateEventActivity extends AppCompatActivity {
     ImageButton eAddImage;
     ImageView eImage;
     Uri picUri;
+    Spinner categorySpinner;
+   // FloatingActionButton myFab;
 
     final int PIC_CROP = 2;
+    final int REQUEST_IMAGE_CAPTURE = 1;
     final int PIC_SAVE = 0;
 
     /**
@@ -93,7 +105,14 @@ public class CreateEventActivity extends AppCompatActivity {
         eName = (EditText) findViewById(R.id.input_name);
         eDescription = (EditText) findViewById(R.id.input_description);
         eAddImage = (ImageButton) findViewById(R.id.addImageButton);
-        //eImage = (ImageView) findViewById(R.id.eventImage);
+        eImage = (ImageView) findViewById(R.id.eventImage);
+        categorySpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
+        categorySpinner.setOnItemSelectedListener(this);
+        //myFab = (FloatingActionButton) findViewById(R.id.fab);
 
 
         eDate.setOnClickListener(new android.view.View.OnClickListener() {
@@ -117,7 +136,8 @@ public class CreateEventActivity extends AppCompatActivity {
                     }
                 }, mYear, mMonth, mDay);
                 mDatePicker.setTitle("Select Date");
-
+                //mDatePicker.getDatePicker().setMinDate(mcurrentDate.getTimeInMillis());
+                mDatePicker.getDatePicker().setMinDate(mcurrentDate.getTimeInMillis());
                 mDatePicker.show();
             }
         });
@@ -134,9 +154,12 @@ public class CreateEventActivity extends AppCompatActivity {
                 mTimePicker = new TimePickerDialog(CreateEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        eTime.setText(selectedHour + ":" + selectedMinute);
+                        boolean isPM = (selectedHour >= 12);
+                        eTime.setText(String.format(Locale.US, "%02d:%02d %s",
+                                (selectedHour == 12 || selectedHour == 0) ? 12 : selectedHour % 12, selectedMinute,
+                                isPM ? "PM" : "AM"));
                     }
-                }, hour, minute, true);//Yes 24 hour time
+                }, hour, minute, false);//No to 24 hour time
                 mTimePicker.setTitle("Select Time");
                 mTimePicker.show();
             }
@@ -160,11 +183,14 @@ public class CreateEventActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int item) {
                 if (items[item].equals("Take Photo")) {
-
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
                 } else if (items[item].equals("Choose from Library")) {
-                    Intent intent = new Intent(Intent.ACTION_PICK,
+                    Intent saveImageIntent = new Intent(Intent.ACTION_PICK,
                             MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(intent, PIC_SAVE);
+                    startActivityForResult(saveImageIntent, PIC_SAVE);
                 } else if (items[item].equals("Cancel")) {
                     dialog.dismiss();
                 }
@@ -184,20 +210,88 @@ public class CreateEventActivity extends AppCompatActivity {
                 performCrop(picUri);
             }
             else if(requestCode == PIC_CROP){
-                //Bundle extras = data.getExtras();
-                //Bitmap thePic = extras.getParcelable("data");
-                Bitmap thePic = null;
+                /*Bitmap pic = null;
                 try {
-                    thePic = MediaStore.Images.Media.getBitmap(
+                    pic = MediaStore.Images.Media.getBitmap(
                             getContentResolver(), picUri);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                eAddImage.setScaleType(ImageView.ScaleType.FIT_XY);
-                //eAddImage.setScaleY(400);
-                eAddImage.setImageBitmap(thePic);
+                eAddImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                eAddImage.setImageBitmap(pic);*/
+                Bundle extras = data.getExtras();
+                Bitmap photo = extras.getParcelable("data");
+                eAddImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                eAddImage.setImageBitmap(photo);
+            }
+            else if(requestCode == REQUEST_IMAGE_CAPTURE){
+                //Bundle extras = data.getExtras();
+                //Bitmap pic = (Bitmap) extras.get("data");
+                picUri = data.getData();
+                performCrop(picUri);
             }
         }
+    }
+
+    private Bitmap normalizeOrientation(Uri imgUri){
+        String filename = getFileName(imgUri);
+        try {
+            ExifInterface ei = new ExifInterface(filename);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    1);
+            System.out.println(orientation);
+            Bitmap pic = MediaStore.Images.Media.getBitmap(
+                    getContentResolver(), picUri);
+
+            switch(orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotateImage(pic, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotateImage(pic, 180);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotateImage(pic, 270);
+                    break;
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    break;
+            }
+            return pic;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix,
+                true);
     }
 
     private void performCrop(Uri picUri) {
@@ -205,21 +299,32 @@ public class CreateEventActivity extends AppCompatActivity {
         Intent cropIntent = new Intent("com.android.camera.action.CROP");
 
         //indicate image type and Uri
-        cropIntent.setDataAndType(picUri,"image/*");
+        cropIntent.setDataAndType(picUri, "image/*");
         //set crop properties
         cropIntent.putExtra("crop", "true");
         //indicate aspect of desired crop
-        cropIntent.putExtra("aspectX", 1);
-        cropIntent.putExtra("aspectY", 1);
+        cropIntent.putExtra("aspectX", 1080);
+        cropIntent.putExtra("aspectY", 1080);
         //indicate output X and Y
         cropIntent.putExtra("outputX", 1080);
         cropIntent.putExtra("outputY", 1080);
         //retrieve data on return
-        //cropIntent.putExtra("scale", true);
-        //cropIntent.putExtra("return-data", true);
+        cropIntent.putExtra("scale", true);
+        cropIntent.putExtra("return-data", true);
         //start the activity - we handle returning in onActivityResult
 
-        cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
+        //cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
+
         startActivityForResult(cropIntent, PIC_CROP);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
