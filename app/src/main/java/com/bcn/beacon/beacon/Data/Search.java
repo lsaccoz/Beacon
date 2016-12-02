1250278b6225504f0427998c7565b0a0d93cd615
Luigi <luigisacco7@gmail.com>
2016-12-02 11:54:38 -0800

    fixed description null bug

--- app/src/main/java/com/bcn/beacon/beacon/Data/Search.java
+++ app/src/main/java/com/bcn/beacon/beacon/Data/Search.java
@@ -10,27 +10,39 @@ public class Search {
     public static ArrayList<ListEvent> searchEvents(String query, List<ListEvent> events) {
         ArrayList<ListEvent> queries = new ArrayList<>();
         query = query.toLowerCase();
+
         for (ListEvent e : events)
+        {
             if (e.getName().toLowerCase().contains(query))
+            {
                 queries.add(e);
+            }
+        }
 
         // if query doesn't find an exact match, look for typos
         if (queries.isEmpty()) {
 
             for (ListEvent e : events)
+            {
                 for (String typo : StringAlgorithms.getStringTypos(query))
-                    if (e.getName().toLowerCase().contains(typo)) {
+                {
+                    if (e.getName().toLowerCase().contains(typo))
+                    {
                         queries.add(e);
                         break;
                     }
+            }}
 
             // if query with typos doesn't find matches,
             // look for only exact matches in descriptions
             if (queries.isEmpty()) {
 
                 for (ListEvent e : events)
-                    if (e.getDescription().toLowerCase().contains(query))
-                        queries.add(e);
+                {
+                    if (e.getDescription() != null) {
+                        if (e.getDescription().toLowerCase().contains(query)) queries.add(e);
+                    }
+                }
             }
         }
 
